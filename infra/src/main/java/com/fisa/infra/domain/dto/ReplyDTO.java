2c391e9cffed776c88ab67c2d434ddbe11f28d26
hwiju yeom <yeom456@gmail.com>
2024-01-08 20:09:05 +0900

    refactor: 해당 부분 다시 수정 예정

---
+++ infra/src/main/java/com/fisa/infra/domain/dto/ReplyDTO.java
@@ -0,0 +1,60 @@
+package com.fisa.infra.domain.dto;
+
+import java.time.LocalDateTime;
+
+import com.fisa.infra.domain.entity.Reply;
+
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+
+@Builder
+public class ReplyDTO {
+
+	//대댓글아이디
+		@Id
+		private String replyId;
+		
+		//댓글아이디
+		private String commentId;
+		
+		//게시글아이디
+		private String boardId; 
+		
+		//회원아이디
+		private String accountId;
+		
+		//내용
+		private String content;
+		
+		//생성날짜
+		private LocalDateTime createdAt;
+		
+		//수정날짜
+		private LocalDateTime updatedAt;
+		
+		//그릅
+		private int groupIndex;
+		
+		//계층
+		private int hierarchyIndex;
+		
+		//순서
+		private int seqIndex;
+		
+		//삭제여부
+		private boolean deleteYN;
+		
+
+		
+}
