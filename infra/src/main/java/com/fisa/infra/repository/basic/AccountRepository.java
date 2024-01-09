a81f546c418e3ff85083d03eb3fdb9024f1bb98d
hwiju yeom <yeom456@gmail.com>
2024-01-09 17:36:07 +0900

    feat: 기본 구조 수정

--- infra/src/main/java/com/fisa/infra/repository/basic/AccountRepository.java
+++ infra/src/main/java/com/fisa/infra/repository/basic/AccountRepository.java
@@ -2,10 +2,11 @@ package com.fisa.infra.repository.basic;
 
 
 import com.fisa.infra.domain.entity.Account;
+import com.fisa.infra.repository.common.CommonAccountRepository;
 import org.springframework.data.repository.Repository;
 
 
-public interface AccountRepository extends Repository<Account,Long> {
+public interface AccountRepository extends Repository<Account,Long>, CommonAccountRepository {
 
 }
 
--- infra/src/main/java/com/fisa/infra/repository/basic/BoardRepository.java
+++ infra/src/main/java/com/fisa/infra/repository/basic/BoardRepository.java
@@ -1,11 +1,11 @@
 package com.fisa.infra.repository.basic;
 
+import com.fisa.infra.repository.common.CommonBoardRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.fisa.infra.domain.entity.Board;
+import org.springframework.data.repository.Repository;
 
-@Repository
-public interface BoardRepository extends JpaRepository<Board,Long>{
+public interface BoardRepository extends Repository<Board,Long>, CommonBoardRepository {
 
 }
