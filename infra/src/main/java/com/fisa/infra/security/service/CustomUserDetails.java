84f899aa74401b814b0a90503cb0824e6bd1d51a
hwiju yeom <yeom456@gmail.com>
2024-01-19 17:38:01 +0900

    feat: Provider에서 맞는 정보인지를 확인하기 위해서 해당 객체 생성

--- infra/src/main/java/com/fisa/infra/security/service/CustomUserDetails.java
+++ infra/src/main/java/com/fisa/infra/security/service/CustomUserDetails.java
@@ -4,40 +4,63 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.List;
+
+import com.fisa.infra.account.domain.entity.Account;
+import com.fisa.infra.role.domain.entity.Role;
 
 public class CustomUserDetails implements UserDetails {
+
+    private final String accountId;
+    private final String accountName;
+    private final String loginId;
+    private final String pwd;
+    private boolean isDeleted;
+    private Collection<GrantedAuthority> authorities;
+
+
+    public CustomUserDetails(Account account){
+        this.accountId = String.valueOf(account.getAccountId());
+        this.accountName = account.getName();
+        this.loginId = account.getLoginId();
+        this.pwd= account.getPwd();
+        this.isDeleted = account.getIsDeleted();
+
+
+    }
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return this.authorities;
     }
 
     @Override
     public String getPassword() {
-        return null;
+        return this.pwd;
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return this.loginId;
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return this.isDeleted;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return this.isDeleted;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return this.isDeleted;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return this.isDeleted;
     }
 }
