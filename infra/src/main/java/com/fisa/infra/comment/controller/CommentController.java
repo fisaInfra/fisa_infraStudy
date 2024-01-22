b8cd05774edffd439216133be23e44b06f61d02a
hwiju yeom <yeom456@gmail.com>
2024-01-22 13:46:20 +0900

    rename: 패키지 변경

--- infra/src/main/java/com/fisa/infra/comment/controller/CommentController.java
+++ infra/src/main/java/com/fisa/infra/comment/controller/CommentController.java
@@ -1,16 +1,9 @@
 package com.fisa.infra.comment.controller;
 
-import com.fisa.infra.account.domain.Account;
-import com.fisa.infra.comment.domain.Comment;
-import com.fisa.infra.comment.domain.CommentDTO;
 import com.fisa.infra.comment.service.CommentService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.PostMapping;
+
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
