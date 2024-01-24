4ae2213b8e37745e6873023443fe43463a1c1362
hwiju yeom <yeom456@gmail.com>
2024-01-24 10:27:56 +0900

    feat: 자동 값 주입을 위한 방법 수정

--- infra/src/main/java/com/fisa/infra/InfraApplication.java
+++ infra/src/main/java/com/fisa/infra/InfraApplication.java
@@ -2,9 +2,10 @@ package com.fisa.infra;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class InfraApplication {
 
     public static void main(String[] args) {
