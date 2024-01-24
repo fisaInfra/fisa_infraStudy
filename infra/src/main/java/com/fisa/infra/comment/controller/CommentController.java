3172ffb23315e24b8818121c16d5a34c7d49d63c
parfait2 <parkyl0208@naver.com>
2024-01-24 14:11:27 +0900

    Feat: 댓글 조회 기능 RMOZ-32

--- infra/src/main/java/com/fisa/infra/comment/controller/CommentController.java
+++ infra/src/main/java/com/fisa/infra/comment/controller/CommentController.java
@@ -1,18 +1,13 @@
 package com.fisa.infra.comment.controller;
 
-import com.fisa.infra.account.domain.Account;
-import com.fisa.infra.comment.domain.Comment;
 import com.fisa.infra.comment.dto.CommentDTO;
 import com.fisa.infra.comment.service.CommentService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -25,7 +20,6 @@ public class CommentController {
     /**
      * 댓글 작성
      * @param commentDTO
-     * @return ResponseEntity
      * http://localhost:8080/swagger-ui/index.html -> 스웨거
      */
     @PostMapping(value = "/create")
@@ -34,8 +28,26 @@ public class CommentController {
         //Account account = (Account) authentication.getPrincipal();
 
         try {
-            Comment comment = commentService.writeComment(commentDTO);
+            commentService.writeComment(commentDTO);
             return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            return ResponseEntity
+                    .internalServerError() // Error 500
+                    .body(e.getMessage());
+        }
+    }
+
+    /**
+     * 댓글 조회
+     * @param boardId
+     * @return result
+     * */
+    @GetMapping(value="/read")
+    public ResponseEntity<?> readComment(@RequestParam Long boardId) {
+        try {
+            List<CommentDTO> result = commentService.readComment(boardId);
+
+            return ResponseEntity.ok().body(result);
         } catch (Exception e) {
             return ResponseEntity
                     .internalServerError() // Error 500
