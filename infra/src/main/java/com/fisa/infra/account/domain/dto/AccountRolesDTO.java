369cfa912fa3a2cf8abb0cb41ff7008faac17f87
hwiju yeom <yeom456@gmail.com>
2024-01-25 13:15:20 +0900

    feat: security 로직 변경

--- infra/src/main/java/com/fisa/infra/account/domain/dto/AccountRolesDTO.java
+++ infra/src/main/java/com/fisa/infra/account/domain/dto/AccountRolesDTO.java
@@ -2,23 +2,19 @@ package com.fisa.infra.account.domain.dto;
 
 import java.util.List;
 
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-public class AccountRoleDTO {
+public class AccountRolesDTO {
 
 	private Long accountId;
 	private String accountName;
 	private String loginId;
 	private String pwd;
 	private boolean isDeleted;
-
-	private String roleName;
+	private List<String> roleName;
 
 }
--- infra/src/main/java/com/fisa/infra/role/repository/querydsl/impl/QueryAccountRoleRepositoryImpl.java
+++ infra/src/main/java/com/fisa/infra/role/repository/querydsl/impl/QueryAccountRoleRepositoryImpl.java
@@ -1,10 +1,13 @@
 package com.fisa.infra.role.repository.querydsl.impl;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.stereotype.Repository;
 
 import com.fisa.infra.account.domain.dto.AccountRoleDTO;
+import com.fisa.infra.role.domain.dto.RoleNameDTO;
+import com.fisa.infra.role.domain.entity.AccountRole;
 import com.fisa.infra.role.domain.entity.QAccountRole;
 import com.fisa.infra.role.repository.querydsl.inter.QueryAccountRoleRepository;
 import com.querydsl.core.types.Projections;
@@ -14,25 +17,21 @@ import lombok.RequiredArgsConstructor;
 
 @Repository
 @RequiredArgsConstructor
-public class QueryAccountRoleRoleRepositoryImpl implements QueryAccountRoleRepository {
+public class QueryAccountRoleRepositoryImpl implements QueryAccountRoleRepository {
 
 	private final JPAQueryFactory factory;
 
 	@Override
-	public Optional<AccountRoleDTO> findAccountRoleByLoginId(String loginId) {
+	public List<RoleNameDTO> findAccountRoleByLoginId(String loginId) {
 
 		QAccountRole accountRole = QAccountRole.accountRole;
 
-
-		return Optional.ofNullable(factory
-			.select(Projections.constructor(AccountRoleDTO.class,
-				accountRole.account.accountId,
-				accountRole.account.name.as("accountName"),
-				accountRole.account.loginId,
-				accountRole.account.pwd,
-				accountRole.role.name.as("roleName")
-				))
+		return factory
+			.select(Projections.constructor(RoleNameDTO.class,accountRole.role.name.as("roleName")))
 			.from(accountRole)
-			.where(accountRole.account.loginId.eq(loginId)).fetchOne());
+			.where(accountRole.account.loginId.eq(loginId)).fetch();
+
 	}
+
+
 }
